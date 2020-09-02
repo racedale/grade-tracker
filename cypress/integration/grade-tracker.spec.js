eee066eb16ccf71ad5140ef2f1aecd00da4e9ff2
Dale Race <dale.race@slalom.com>
2020-09-01 19:47:16 -0500

    jQuery # for id

--- cypress/integration/grade-tracker.spec.js
+++ cypress/integration/grade-tracker.spec.js
@@ -46,7 +46,7 @@ context('Navigation', () => {
       // to find which element was created
       const announcements = $body.find('[role="dialog"]')
       if (announcements.length > 0) {
-        $body.find('$cancelButton').click()
+        $body.find('#cancelButton').click()
       }
     })
     cy.findByText('Current Grades')
